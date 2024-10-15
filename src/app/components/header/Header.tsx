506a67715b051ee91f01bf2b2d6f4526d1858321
Nweke Chidi <nwekechidigodwin460@gmail.com>
2024-10-15 18:11:18 +0100

    "Added react-icons dependency, updated Header component with mobile navigation, and modified Hero component's content and styling."
    Signed-off-by: Nweke Chidi <nwekechidigodwin460@gmail.com>

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 interface FooterProps {
   handleNavigation: (section: string) => void;
 }
 
 const Header = ({ handleNavigation }: FooterProps) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <header className="sticky z-20 top-0 h-[80px] md:h-[96px] py-6 md:py-7  ">
-      <div className="w-11/12 max-w-screen-xl mx-auto  flex flex-row justify-between items-center">
+    <header className="sticky z-20 top-0 h-[80px] md:h-[96px] py-6 md:py-7 bg-[#FBFFFF] backdrop-blur-3xl bg-opacity-100">
+      <div className="w-11/12 max-w-screen-xl mx-auto bg-[#FBFFFF] flex flex-row justify-between items-center">
         <Link href="/">
           <Image
-            src="/img/powerline-logo.png"
+            src="/img/powerline-logo.svg"
             alt="Hivenify Logo"
             width={241}
             height={52}
@@ -20,21 +27,21 @@ const Header = ({ handleNavigation }: FooterProps) => {
           />
         </Link>
 
-        <ul className="hidden md:flex flex-row gap-6 text-hiven-ash hover:text-hiven-ash/90 items-center justify-center text-sm font-medium">
+        <ul className="hidden md:flex flex-row gap-8 items-center justify-center text-sm font-medium">
           <li
-            className="hover:text-hiven-black cursor-pointer"
+            className="hover:text-black/70 cursor-pointer"
             onClick={() => handleNavigation("howItWorksRef")}
           >
             How it Works
           </li>
           <li
-            className="hover:text-hiven-black cursor-pointer"
+            className="hover:text-black/70 cursor-pointer"
             onClick={() => handleNavigation("testimonialRef")}
           >
             Testimonials
           </li>
           <li
-            className="hover:text-hiven-black cursor-pointer"
+            className="hover:text-black/70 cursor-pointer"
             onClick={() => handleNavigation("contactUsRef")}
           >
             Contact Us
@@ -42,18 +49,108 @@ const Header = ({ handleNavigation }: FooterProps) => {
         </ul>
 
         {/* Power Line Socials */}
-        <div className="flex flex-row gap-4 items-center">
-          <Link href={"/auth/login"}>
-            <button className="text-hiven-ash hover:text-[#1D2571] font-medium text-xs md:text-sm">
-              Sign in
-            </button>
+        <div className="hidden md:flex flex-row gap-4 items-center">
+          <Link href={"/"}>
+            <Image
+              src="/icons/x.svg"
+              alt="Twitter (X) Logo"
+              width={48}
+              height={48}
+              className="w-[48px] h-[48px]"
+            />
           </Link>
-          <Link
-            href={"/auth/signup"}
-            className="font-semibold text-xs md:text-sm text-white bg-hiven-blue hover:bg-[#1D2571]   py-2 md:py-3 px-3 md:px-5 rounded-lg cursor-pointer"
-          >
-            <button>Sign up</button>
+          <Link href={"/"}>
+            <Image
+              src="/icons/facebook.svg"
+              alt="Facebook Logo"
+              width={48}
+              height={48}
+              className="w-[48px] h-[48px]"
+            />
           </Link>
+          <Link href={"/"}>
+            <Image
+              src="/icons/instagram.svg"
+              alt="Instagram Logo"
+              width={48}
+              height={48}
+              className="w-[48px] h-[48px]"
+            />
+          </Link>
+        </div>
+
+        {/* Hamburger Icon for Mobile */}
+        <button
+          className="md:hidden  focus:outline-none"
+          onClick={toggleMenu}
+          aria-label="Toggle Menu"
+        >
+          {menuOpen ? <FaTimes size={20} /> : <FaBars size={20} />}
+        </button>
+
+        <div
+          className={`block md:hidden fixed top-[80px] left-0 w-full h-[240px] bg-[#FBFFFF] py-4 z-10 transform ${
+            menuOpen ? "translate-x-0" : "translate-x-full"
+          } transition-transform duration-300 ease-in-out`}
+        >
+          <ul className="flex flex-col items-center gap-6 mt-0 text-hiven-ash text-lg font-medium">
+            <li
+              className="hover:text-black/50 cursor-pointer"
+              onClick={() => {
+                handleNavigation("howItWorksRef");
+                toggleMenu();
+              }}
+            >
+              How it Works
+            </li>
+            <li
+              className="hover:text-black/50 cursor-pointer"
+              onClick={() => {
+                handleNavigation("testimonialRef");
+                toggleMenu();
+              }}
+            >
+              Testimonials
+            </li>
+            <li
+              className="hover:text-black/50 cursor-pointer"
+              onClick={() => {
+                handleNavigation("contactUsRef");
+                toggleMenu();
+              }}
+            >
+              Contact Us
+            </li>
+            <li className="flex md:hidden flex-row gap-4 items-center">
+              <Link href={"/"}>
+                <Image
+                  src="/icons/x.svg"
+                  alt="Twitter (X) Logo"
+                  width={36}
+                  height={36}
+                  className="w-[36px] h-[36px]"
+                />
+              </Link>
+              <Link href={"/"}>
+                <Image
+                  src="/icons/facebook.svg"
+                  alt="Facebook Logo"
+                  width={36}
+                  height={36}
+                  className="w-[36px] h-[36px]"
+                />
+              </Link>
+              <Link href={"/"}>
+                <Image
+                  src="/icons/instagram.svg"
+                  alt="Instagram Logo"
+                  width={36}
+                  height={36}
+                  className="w-[36px] h-[36px]"
+                />
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
     </header>
--- src/app/components/home/Hero.tsx
+++ src/app/components/home/Hero.tsx
@@ -15,8 +15,8 @@ const Hero = React.forwardRef<HTMLElement, {}>((props, ref) => {
         />
       </div>
       {/* Content over the image */}
-      <div className="relative z-10 text-white text-center pt-40  py-10">
-        <h1 className="text-4xl font-bold">Welcome to Hivenify</h1>
+      <div className="w-11/12 max-w-screen-xl mx-auto py-[70px] relative z-10 text-white text-center ">
+        <h1 className="text-4xl font-bold">Welcome to Power Line</h1>
         <p className="mt-4">Your journey starts here</p>
       </div>
     </section>
